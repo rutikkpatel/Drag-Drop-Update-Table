5e73729c3d26b172733ab59746fc3ff1685cedd3
rutikpatel20 <rutik.p@simformsolutions.com>
2023-08-06 01:42:57 +0530

    add jQuery and jQuery UI sortable in project

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "jquery-ui-dist"
 import "@hotwired/turbo-rails"
 import "controllers"
